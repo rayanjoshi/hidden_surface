30f01dde5269fa1cec32e97603c3add38f339acd
Rayan Joshi <rj524@ic.ac.uk>
2025-11-03 23:59:09 +0000

    fix: update TSLA option data query to cover 2019-2022 instead of 2020-2023

--- src/option_data_query.sql
+++ src/option_data_query.sql
@@ -3,7 +3,7 @@
 -- Uses UNION ALL to combine data from multiple yearly tables to exhaust all options
 -- Added Fama-French 3 Factors Plus Momentum (Mkt-RF, SMB, HML, UMD) and Risk-Free Rate (RF) from ff.factors_daily
 
--- 2020 Data
+-- 2019 Data
 SELECT
     o.date,
     o.secid,
@@ -46,6 +46,64 @@ SELECT
     f.umd AS momentum,
     f.rf AS risk_free_rate
 FROM
+    optionm.opprcd2019 o
+LEFT JOIN
+    optionm.secprd2019 u
+    ON o.secid = u.secid AND o.date = u.date
+LEFT JOIN
+    ff.factors_daily f
+    ON o.date = f.date
+WHERE
+    o.secid = 143439  -- TSLA secid
+    AND o.date >= '2019-01-01'
+    AND o.best_bid IS NOT NULL
+    AND o.best_offer IS NOT NULL
+    AND o.impl_volatility IS NOT NULL
+    AND o.impl_volatility > 0
+    AND o.strike_price > 0
+
+UNION ALL
+
+-- 2020 Data
+SELECT
+    o.date,
+    o.secid,
+    o.optionid,
+    o.symbol,
+    o.cp_flag,
+    o.strike_price / 1000.0 AS strike_price,
+    o.exdate AS expiration,
+    o.best_bid,
+    o.best_offer,
+    (o.best_bid + o.best_offer) / 2.0 AS mid_quote,
+    o.volume,
+    o.open_interest,
+    o.impl_volatility AS implied_volatility,
+    o.delta,
+    o.gamma,
+    o.theta,
+    o.vega,
+    o.cfadj,
+    o.am_settlement,
+    o.contract_size,
+    u.close AS underlying_close,
+    u.open AS underlying_open,
+    u.high AS underlying_high,
+    u.low AS underlying_low,
+    u.volume AS underlying_volume,
+    (o.exdate::date - o.date::date) / 365.25 AS time_to_expiration,
+    (o.strike_price / 1000.0) / u.close AS moneyness,
+    CASE 
+        WHEN o.best_bid > 0 AND o.best_offer > 0 AND o.best_offer > o.best_bid 
+        THEN 1 
+        ELSE 0 
+    END AS valid_pricing,
+    f.mktrf AS market_excess_return,
+    f.smb AS smb,
+    f.hml AS hml,
+    f.umd AS momentum,
+    f.rf AS risk_free_rate
+FROM
     optionm.opprcd2020 o
 LEFT JOIN
     optionm.secprd2020 u
@@ -54,8 +112,7 @@ LEFT JOIN
     ff.factors_daily f
     ON o.date = f.date
 WHERE
-    o.secid = 143439  -- TSLA secid
-    AND o.date >= '2020-01-01'
+    o.secid = 143439
     AND o.best_bid IS NOT NULL
     AND o.best_offer IS NOT NULL
     AND o.impl_volatility IS NOT NULL
@@ -170,64 +227,7 @@ LEFT JOIN
     ON o.date = f.date
 WHERE
     o.secid = 143439
-    AND o.best_bid IS NOT NULL
-    AND o.best_offer IS NOT NULL
-    AND o.impl_volatility IS NOT NULL
-    AND o.impl_volatility > 0
-    AND o.strike_price > 0
-
-UNION ALL
-
--- 2023 Data
-SELECT
-    o.date,
-    o.secid,
-    o.optionid,
-    o.symbol,
-    o.cp_flag,
-    o.strike_price / 1000.0 AS strike_price,
-    o.exdate AS expiration,
-    o.best_bid,
-    o.best_offer,
-    (o.best_bid + o.best_offer) / 2.0 AS mid_quote,
-    o.volume,
-    o.open_interest,
-    o.impl_volatility AS implied_volatility,
-    o.delta,
-    o.gamma,
-    o.theta,
-    o.vega,
-    o.cfadj,
-    o.am_settlement,
-    o.contract_size,
-    u.close AS underlying_close,
-    u.open AS underlying_open,
-    u.high AS underlying_high,
-    u.low AS underlying_low,
-    u.volume AS underlying_volume,
-    (o.exdate::date - o.date::date) / 365.25 AS time_to_expiration,
-    (o.strike_price / 1000.0) / u.close AS moneyness,
-    CASE 
-        WHEN o.best_bid > 0 AND o.best_offer > 0 AND o.best_offer > o.best_bid 
-        THEN 1 
-        ELSE 0 
-    END AS valid_pricing,
-    f.mktrf AS market_excess_return,
-    f.smb AS smb,
-    f.hml AS hml,
-    f.umd AS momentum,
-    f.rf AS risk_free_rate
-FROM
-    optionm.opprcd2023 o
-LEFT JOIN
-    optionm.secprd2023 u
-    ON o.secid = u.secid AND o.date = u.date
-LEFT JOIN
-    ff.factors_daily f
-    ON o.date = f.date
-WHERE
-    o.secid = 143439
-    AND o.date <= '2023-12-31'
+    AND o.date <= '2022-12-31'
     AND o.best_bid IS NOT NULL
     AND o.best_offer IS NOT NULL
     AND o.impl_volatility IS NOT NULL
